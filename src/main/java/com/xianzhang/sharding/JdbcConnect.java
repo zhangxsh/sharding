c7129a2147a03df1b0d1ba4ebcfa29a14e486be8
alexzhang <alexzhang@gmail.com>
2020-07-21 09:38:16 +0800

    proxy

---
+++ src/main/java/com/xianzhang/sharding/JdbcConnect.java
@@ -0,0 +1,25 @@
+package com.xianzhang.sharding;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+/**
+ * @author alexzhang
+ * @date 2020/7/21 09:15
+ */
+public class JdbcConnect {
+    public static void main(String[] args) throws Exception {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection con = DriverManager.getConnection("jdbc:mysql://192.168.8.81:13308/sharding_db?characterEncoding=UTF-8","root","root");
+
+        Statement st=con.createStatement();
+        ResultSet rs=st.executeQuery("select * from user_info where uid=1");
+        while(rs.next()) {
+            long id = rs.getLong(1);
+            long age = rs.getLong(2);
+            System.out.println(id);System.out.println(age);
+        }
+    }
+}
